a5e9baa396f81cfa3bd73d7a4db0ed8af6a31459
MikaVarulven <33130238+MikaVarulven@users.noreply.github.com>
2017-12-07 19:20:46 +0100

    Add files via upload

--- display_menu.py
+++ display_menu.py
@@ -1,18 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+display_menu function
+"""
+#---------------------------------------Initialization------------------------------------------------------------
 import numpy as np
 
 
 def input_number(prompt):
+    #input_number prompts user to input a number and assigns value to variable choice.
+    #If it is not the case give error message to user.
+    #Input: from user
+    #Output: number (float)
+    
+    
     try:
         choice=float(input(prompt))
-
-
+        
+        
+    #If input can be converted in a float print messgae
     except ValueError:
-        print("Type in a valid number!\n>>")
+        print("Unfortunately this is not a vaild input.\n>>")
         return 0
     return choice
 
 def display_menu(options):
+    #display_menu takes in an array of options and prints the options while assigning to each option a number(start: 1.). 
+    #uses number input and gives further information to mistake if input was invalid.
+    #Input: from user 
+    #output: number (integer)
+    
     i=0
+    
+    #displaying the options
     for element in options:
 
         print(i+1,".",element,"\n")
@@ -20,13 +39,15 @@ def display_menu(options):
 
     choice=0
 
+    #error check:
+    #check's for type number and to valid numbers
     while not(np.any(choice==np.arange(len(options))+1)):
        choice=input_number("Choose an option\n>>")
+       
        if not (np.any(choice==np.arange(len(options))+1)):
-            print("Please choose one of the options\n>>")
+            print("Please type in a number corresponding to the options\n>>")
 
 
 
     return choice
-
 
